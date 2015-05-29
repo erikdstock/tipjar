bea222d92675fa9c9ca21f107a30b085e1b09175
Erik Stockmeier <erik.d.stock@gmail.com>
2015-05-28 22:50:26 -0500

    remove some unneeded devise capabilities

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,17 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :database_authenticatable, :registerable,
   # :recoverable,  :timeoutable and :omniauthable
-  devise :rememberable, :trackable, :validatable 
+
+  ## Commenting the first devise line out because i'm getting an email can't be blank validation
+  # devise :rememberable, :trackable, :validatable 
   devise :omniauthable, :omniauth_providers => [:lastfm]
 
   def self.from_omniauth(auth)
 	  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+	  	# byebug
 	    user.password = Devise.friendly_token[0,20]
-	    user.email 		= auth.info.email
-	    user.name			= auth.info.name
+	    user.email 		= auth.info.email 
+	    user.name			= auth.info.name 
 	    user.image 		= auth.info.image # assuming the user model has an image
 	  end
 	end
