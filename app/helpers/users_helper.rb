3af3688e71c660aa6f26846ac9c4eebc2b758249
Chase Anthony Rogers <chase@bellwethermade.com>
2016-06-08 17:52:56 -0500

    refactor user model to use helper methods

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,33 @@
 module UsersHelper
+  extend self
+
+  def validate_time(time)
+    raise 'month is in the future' if month_start(time).future?
+  end
+
+  def time_range_month(time)
+    validate_time(time)
+    month_start(time)..month_end(time)
+  end
+
+  def month_start(time)
+    DateTime.new(time.year, time.month, 1).utc
+  end
+
+  def month_end(time)
+    DateTime.new(time.year, time.month, -1, -1, -1).utc
+  end
+
+  def top_artist_attributes
+    <<-STRING
+      artists.name,
+      artists.image,
+      month,
+      play_count,
+      user_id,
+      artist_id,
+      monthly_top_artists.updated_at
+    STRING
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,46 +16,17 @@ class User < ActiveRecord::Base
   end
 
   def monthly_top_artists(time)
-    time_range = time_range_month(time)
+    time_range = UsersHelper.time_range_month(time)
     artists = current_top_artist(time_range)
-    artists if !month_end(time).future? && !artists.empty?
+    artists if !UsersHelper.month_end(time).future? && !artists.empty?
   end
 
   private
 
-  def validate_time(time)
-    raise 'month is in the future' if month_start(time).future?
-  end
-
-  def time_range_month(time)
-    validate_time(time)
-    month_start(time)..month_end(time)
-  end
-
-  def month_start(time)
-    DateTime.new(time.year, time.month, 1).utc
-  end
-
-  def month_end(time)
-    DateTime.new(time.year, time.month, -1, -1, -1).utc
-  end
-
-  def top_artist_attributes
-    <<-STRING
-      artists.name,
-      artists.image,
-      month,
-      play_count,
-      user_id,
-      artist_id,
-      monthly_top_artists.updated_at
-    STRING
-  end
-
   def current_top_artist(time_range)
     MonthlyTopArtist.where(user_id: id, month: time_range)
     .joins(:artist)
-    .select(top_artist_attributes)
+    .select(UsersHelper.top_artist_attributes)
   end
 
 end
