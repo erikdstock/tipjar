52d5c1c5e6b3f47798962c929b951063e125f5af
Chase Anthony Rogers <chase@bellwethermade.com>
2016-06-09 15:49:48 -0500

    refactor helper tests to be more explicit

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe UsersHelper, type: :helper do
 
   context '#top_artist_attributes' do
     it "returns a string containing the desired attributes" do
-      expect(helper.top_artist_attributes).to be_a String
-      expect(helper.top_artist_attributes).to eq(
+      expect(UsersHelper.top_artist_attributes).to be_a String
+      expect(UsersHelper.top_artist_attributes).to eq(
         "artists.name, artists.image, month, play_count, " +
         "user_id, artist_id, monthly_top_artists.updated_at"
       )
@@ -16,30 +16,30 @@ RSpec.describe UsersHelper, type: :helper do
     it "raises the proper error if the month begins in the future" do
       time = DateTime.new(2017, 3, 9)
       message = 'month is in the future'
-      expect{helper.validate_time(time)}.to raise_error(RuntimeError, message)
+      expect{UsersHelper.validate_time(time)}.to raise_error(RuntimeError, message)
     end
   end
 
   context '#time_range_month' do
     it 'returns a range' do
       time = DateTime.new(2017, 3, 9)
-      expect(helper.time_range_month(time)).to be_a Range
+      expect(UsersHelper.time_range_month(time)).to be_a Range
     end
   end
 
   context '#month_start' do
     it 'returns a DateTime object with the first day of the month' do
       time = DateTime.new(2017, 3, 9)
-      expect(helper.month_start(time)).to be_a DateTime
-      expect(helper.month_start(time)).to eq "Wed, 01 Mar 2017 00:00:00.000000000 +0000"
+      expect(UsersHelper.month_start(time)).to be_a DateTime
+      expect(UsersHelper.month_start(time)).to eq "Wed, 01 Mar 2017 00:00:00.000000000 +0000"
     end
   end
 
   context '#month_end' do
     it 'returns a DateTime object with the last day of the month' do
       time = DateTime.new(2017, 3, 9)
-      expect(helper.month_end(time)).to be_a DateTime
-      expect(helper.month_end(time)).to eq "Fri, 31 Mar 2017 23:59:00.000000000 +0000"
+      expect(UsersHelper.month_end(time)).to be_a DateTime
+      expect(UsersHelper.month_end(time)).to eq "Fri, 31 Mar 2017 23:59:00.000000000 +0000"
     end
   end
 
