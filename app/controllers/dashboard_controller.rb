e4def5c4a80c24f9f915e67a82c0695a66d42690
Erik Stockmeier <erik.d.stock@gmail.com>
2016-07-03 12:04:00 -0500

    add dashbaord files

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,10 @@
+class DashboardController < ApplicationController
+  before_action :authenticate_user!
+
+  def main
+    @month = 1.month.ago
+    @user = current_user
+    @top_artists = @user.top_artists_for_month(@month)
+  end
+
+end
