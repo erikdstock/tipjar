d359bd040efde4a411075b48b464588d7e0c8bfe
Erik Stockmeier <erik.d.stock@gmail.com>
2016-07-03 19:27:04 -0500

    begin dockerize

--- code/config/initializers/sidekiq.rb
+++ code/config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
+sidekiq_config = { url: ENV['JOB_WORKER_URL'] }
+
 Sidekiq.configure_server do |config|
-    config.redis = { url: 'redis://localhost:6379/0', namespace: "sidekiq_app_name_#{Rails.env}" }
+  config.redis = sidekiq_config
 end
 
 Sidekiq.configure_client do |config|
-    config.redis = { url: 'redis://localhost:6379/0', namespace: "sidekiq_app_name_#{Rails.env}" }
-end 
+  config.redis = sidekiq_config
+end
