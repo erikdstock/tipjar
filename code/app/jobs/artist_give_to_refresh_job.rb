0c4bd5c821324fb35a5c0bb8597aa75cd78c1f8e
Erik Stockmeier <erik.d.stock@gmail.com>
2016-07-17 09:40:11 -0500

    refactor give to refresh job

--- code/app/jobs/artist_give_to_refresh_job.rb
+++ code/app/jobs/artist_give_to_refresh_job.rb
@@ -2,9 +2,9 @@ class ArtistGiveToRefreshJob < ActiveJob::Base
   # queue_as :give_to
 
   # this method serializes the incoming data to redis so only use artist_id, not whole object
-  def perform(artist_id, *args)
-    artist = Artist.find(artist_id)
-    logger.info "checking give.to data for artist #{artist.name}"
+  def perform(artist_id, *_args)
+    artist = Artist.find_by(id: artist_id)
+    return nil unless artist
     if artist.give_to_incomplete?
       give_to_data = GiveToApi.new.check_artist(artist.name)
       logger.info "updating:"
@@ -16,7 +16,5 @@ class ArtistGiveToRefreshJob < ActiveJob::Base
         return false
       end
     end
-    logger.info "#{artist.name}'s data is up to date"
-    true
   end
 end
