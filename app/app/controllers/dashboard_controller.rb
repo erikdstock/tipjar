103c4ac2aeaf9869da34cb26f52e6c95472659b8
Erik Stockmeier <erik.d.stock@gmail.com>
2017-03-29 08:44:15 -0400

    almost rewired devise

--- app/app/controllers/dashboard_controller.rb
+++ app/app/controllers/dashboard_controller.rb
@@ -1,4 +1,22 @@
 class DashboardController < ApplicationController
-  def index
+  before_action :authenticate_user!
+
+  def main
+    @month = 1.month.ago
+    @user = current_user
+    artists = @user.top_artists_for_month(@month)
+    # TODO: EXTRACT THIS
+    @formatted_artists = artists.map do |artist|
+      {
+        name: artist.artist.name,
+        artist_id: artist.artist_id,
+        play_count: artist.play_count,
+        image: artist.image,
+        month: artist.month,
+        give_to_url: artist.artist.give_to_url,
+        give_to_verified: artist.artist.give_to_verified
+      }
+    end
+    @top_artists = { month: format_month_year(@month), artists: @formatted_artists }
   end
 end
--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,23 +1,27 @@
 Rails.application.routes.draw do
-
   root 'dashboard#main'
   get 'dashboard', to: 'dashboard#main', as: :user_dashboard
 
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+  # devise_scope :user do
+  #   get 'sign_in', to: 'devise/sessions#new'
+  # end
   devise_scope :user do
-    get 'sign_in', to: 'devise/sessions#new'
+    get 'sign_in', to: 'users/sessions#new', as: :new_user_session
+    post 'sign_in' => 'users/sessions#create', :as => :user_session
+    get 'sign_out', to: 'users/sessions#destroy', as: :destroy_user_session
   end
-
-
   namespace :api, defaults: { format: :json } do
-    get '/', to: 'root#main', as: :api_root
+    get '/', to: 'root#main', as: :root
     # root 'welcome#index'
     # resources :artists, only: [:index]
     # resources :users
     # get 'top-artists', to: 'users#top_artists', as: :top_artists
     # get 'recent-tracks', to: 'users#recent_tracks', as: :recent_tracks
   end
-
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
