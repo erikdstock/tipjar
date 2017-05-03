872a7a0414b91ad65861d533c60be85ba7673c66
Erik Stockmeier <erik.d.stock@gmail.com>
2017-05-02 23:41:30 -0400

    try a circle config

--- src/app/javascript/components/common/Header.test.jsx
+++ src/app/javascript/components/common/Header.test.jsx
@@ -1,30 +1,25 @@
 import $ from 'teaspoon'
-import ReactTestRenderer from 'react-test-renderer'
-import sinon from 'sinon'
+import React from 'react'
+// import ReactTestRenderer from 'react-test-renderer'
+// import sinon from 'sinon'
+// import ReactDOM
 import Header from 'components/common/Header'
 import { userFixture } from '__test__/helpers/fixtures'
 
 describe('Header', () => {
-  let props
+  let props, element
 
   beforeEach(() => {
-    props = {
-      sale: userFixture(),
-      onRegisterToBid: sinon.stub()
-    }
+    props = {}
+      // user: userFixture(),
+    // }
   })
 
-  it('displays the right message', () => {
-    expect(
-      rtr.render(Header(props))
-        .text()
-      ).toMatch('Log In')
-  })
-
-  xit('calls onRegisterToBid when clicked', () => {
-    expect(context.onRegisterToBid.callCount).toEqual(0)
-    const node = $(Header(props)).find('a').unwrap()
-    node.props.onClick()
-    expect(context.onRegisterToBid.callCount).toEqual(1)
+  describe('not logged in', () => {
+    it('displays the login link', () => {
+      props.isLoggedIn = false
+      element = <Header {...props} />
+      expect($(element).render().text()).toMatch('Log In')
+    })
   })
 })
