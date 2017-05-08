5aeb0b40c13e0be85a8aaaf0ebab5026387d5268
Erik Stockmeier <erik.d.stock@gmail.com>
2017-05-07 22:25:53 -0400

    wtf

--- src/app/javascript/components/common/Header.jsx
+++ src/app/javascript/components/common/Header.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-import { Redirect } from 'react-router'
+import { Redirect, Link } from 'react-router'
 import { deleteSessionJwt } from 'utils/session'
 
 class Header extends React.Component {
-
   render () {
     let sessionLink
     if (this.props.loggedIn) {
-      sessionLink = <a href='#'>Log Out</a>
+      sessionLink = <a href='#' onClick={this.logoutAndRedirect} >Log Out</a>
     } else {
-      sessionLink = <a href='#'>Log In</a>
+      sessionLink = <Link to='/login' >Log In</Link>
     }
 
     return (
@@ -21,13 +20,15 @@ class Header extends React.Component {
     )
   }
 
-  logoutAndRedirect () {
+  logoutAndRedirect (e) {
+    e.preventDefault()
     deleteSessionJwt()
     return <Redirect to='/' />
   }
 }
 
 Header.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired
   // actions: PropTypes.object.isRequired
 }
 
--- src/app/javascript/components/common/Header.test.jsx
+++ src/app/javascript/components/common/Header.test.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 // import sinon from 'sinon'
 // import ReactDOM
 import { Header } from 'components/common/Header'
-import { userFixture } from '__test__/helpers/fixtures'
+// import { userFixture } from '__test__/helpers/fixtures'
 
 describe('Header', () => {
   let props, element
@@ -15,9 +15,20 @@ describe('Header', () => {
     // }
   })
 
+  describe('logged in', () => {
+    beforeEach(() => {
+      props.isLoggedIn = true
+    })
+    it('displays the log out link', () => {
+      element = <Header {...props} />
+      expect($(element).render().text()).toMatch('Log Out')
+    })
+  })
   describe('not logged in', () => {
-    it('displays the login link', () => {
+    beforeEach(() => {
       props.isLoggedIn = false
+    })
+    it('displays the login link', () => {
       element = <Header {...props} />
       expect($(element).render().text()).toMatch('Log In')
     })
